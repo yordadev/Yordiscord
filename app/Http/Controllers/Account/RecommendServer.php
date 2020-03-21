1a9ea71a815f674c96bca674a32b3fb84f9d16b6
yordadev <yordadevs@gmail.com>
2020-03-21 22:05:18 +0000

    added rate limit to recommending server

--- app/Http/Controllers/Account/RecommendServer.php
+++ app/Http/Controllers/Account/RecommendServer.php
@@ -17,17 +17,30 @@ class RecommendServer extends Controller
             'testimony' => 'nullable|string|min:6|max:255'
         ]);
 
-        try {
-            ServerRecommendation::create([
-                'discord_id' => Auth::user()->discord_id,
-                'server_id'  => $request->server_id,
-                'testimony'  => $request->testimony ?? null,
-                'recommended'  => true
-            ]);
-    
-            return redirect()->back()->with('success', 'You have successfully recommended the server.');
-        } catch (\Exception $e){        
-            return redirect()->back()->withErrors(['Something went wrong trying to recommend this server.']);
+        if($this->canRecommend($request->server_id)){
+            try {
+                ServerRecommendation::create([
+                    'discord_id' => Auth::user()->discord_id,
+                    'server_id'  => $request->server_id,
+                    'testimony'  => $request->testimony ?? null,
+                    'recommended'  => true
+                ]);
+        
+                return redirect()->back()->with('success', 'You have successfully recommended the server.');
+            } catch (\Exception $e){        
+                return redirect()->back()->withErrors(['Something went wrong trying to recommend this server.']);
+            }
         }
+        return redirect()->back()->withErrors(['You have already recommended this server, thanks.']);
+    }
+
+    private function canRecommend($server_id){
+        if(ServerRecommendation::where([
+            'discord_id' => Auth::user()->discord_id,
+            'server_id'  => $server_id,
+        ])->first()){
+            return false;
+        }
+        return true;
     }
 }
